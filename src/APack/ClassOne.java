ac5fcf750b4293e145423e888c127c576069da70
PriyaBob <priya172415@gmail.com>
2020-05-16 13:00:08 -0500

    Uploding Local Automation code to git hub

---
+++ src/APack/ClassOne.java
@@ -0,0 +1,16 @@
+package APack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClassOne {
+  @Test
+  public void methodOneA() {
+	  System.out.println("Inside methodOneA of classOne");
+	  System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("http://www.google.com");
+	  driver.close();
+}
+}
---
+++ src/APack/ClassTwo.java
@@ -0,0 +1,22 @@
+package APack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClassTwo {
+
+	@Test
+	public void methodTwoA() {
+System.out.println("Inside methodTwoA of classTwo");
+System.setProperty("webdriver.chrome.driver", "drivers//chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+driver.get("http://www.facebook.com");
+driver.close();
+	}
+	@Test
+	public void methodTwoB() {
+		System.out.println("Inside MethodTwoB of classTwo");
+	}
+
+}
